66264884b3ba4cc1c04e61d0e35f4e73e4efc8c6
Bruno Nascimento <brunomn9@hotmail.com>
2019-08-27 09:21:42 -0300

    Iniciando batedeira

---
+++ src/main/java/poo/Batedeira.java
@@ -0,0 +1,29 @@
+package poo;
+
+public class Batedeira {
+
+    //atributos
+    private int onOff;
+    private int velocidade;
+
+
+//métodos
+
+    public int getOnOff() {
+        return onOff;
+    }
+
+    public void setOnOff(int onOff) {
+        this.onOff = onOff;
+    }
+
+    public void acelerar(){
+        this.velocidade++;
+    }
+
+    public void desacelerar(){
+        this.velocidade--;
+    }
+
+    public void
+}
